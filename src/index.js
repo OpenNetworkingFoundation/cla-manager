4c7d79931c6127dc8d5e54b34d8e195dbf92ab85
Matteo Scandolo <matteo.scandolo@gmail.com>
2019-12-03 16:59:46 -0800

    Adding support for multiple environment (dev purpose)

--- src/index.js
+++ src/index.js
@@ -8,15 +8,22 @@ import 'firebase/firestore';
 import './index.css';
 import AppRouter from './js/AppRouter';
 import { HandleSignInLink } from './js/SignIn';
+import { HandleDevError } from './js/DevError';
+
+if (!process.env.REACT_APP_FIREBASE_ENV || !process.env.REACT_APP_FIREBASE_API_KEY) {
+  console.error("Environments var missing! Please refer to the README file")
+  // TODO render a stupid component
+  HandleDevError(c => ReactDOM.render(c, document.getElementById('root')));
+}
 
 firebase.initializeApp({
-    "apiKey": "AIzaSyDai4LocdZpoa0t219SDqOEQ4ipMQVOVvQ",
-    "databaseURL": "https://cla-manager-bf923.firebaseio.com",
-    "storageBucket": "cla-manager-bf923.appspot.com",
-    "authDomain": "cla-manager-bf923.firebaseapp.com",
+    "apiKey": process.env.REACT_APP_FIREBASE_API_KEY,
+    "databaseURL": `https://${process.env.REACT_APP_FIREBASE_ENV}.firebaseio.com`,
+    "storageBucket": `${process.env.REACT_APP_FIREBASE_ENV}.appspot.com`,
+    "authDomain": `${process.env.REACT_APP_FIREBASE_ENV}.firebaseapp.com`,
     "messagingSenderId": "232849741230",
-    "projectId": "cla-manager-bf923"
-  });
+    "projectId": `${process.env.REACT_APP_FIREBASE_ENV}`,
+});
 //   firebase.firestore(); // initialize the firestore... why? who knows
 //   console.log(firebase.firestore())
 // firebase.firestore.setLogLevel('debug');
