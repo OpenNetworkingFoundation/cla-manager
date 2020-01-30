381d39c2c2b6b63e5df6d61cdedee54908cd944c
Zack Williams <zdw@opennetworking.org>
2020-01-30 13:31:18 -0700

    Gerrit hook for CLA manager

---
+++ gerrit/README.md
@@ -0,0 +1,53 @@
+# Gerrit integration for CLA Manager
+
+The `ref-update` script runs as a gerrit hooks:
+
+https://gerrit.googlesource.com/plugins/hooks/+/master/src/main/resources/Documentation/hooks.md
+
+When run, it queries the CLA server based on the email address given.
+
+If the CLA is signed, it exits with success.
+
+If the CLA is not signed, it returns the message provided by the server which
+Gerrit will send to the user.
+
+It also handles common error cases (can't contact server, invalid response,
+etc.)
+
+
+## Deploying the ref-update script
+
+1. Modify the `CLA_MANAGER_URL` near the top of the `ref-updated` file to match
+   your cla-manager deployment.
+
+2. Test that settings are correct by running the command:
+
+        ./ref-update --uploader "Foo Bar (foo@bar.com)" --debug
+
+   Test with email addresses that pass (has signed) and fail (hasn't signed)
+   cases, checking the exit code (`echo $?`) after each command.
+
+3. Change `ENFORCING` to `False` in the `ref-updated` file - this makes the
+   script always exit with success.
+
+4. Copy to your gerrit instance (default location is in the `$site_path/hooks`
+   directory), and modify the gerrit configuration to use it, if needed:
+   https://gerrit.googlesource.com/plugins/hooks/+/master/src/main/resources/Documentation/config.md
+
+5. Create and submit a few tests commits, checking with users that have both
+   signed and not signed the CLA using cla-manager.  Check that it:
+
+   a. Doesn't send a message when user has signed a CLA
+
+   b. Sends a message when the CLA hasn't been signed
+
+6. Change `ENFORCING` to `True` in the `ref-updated` file.
+
+## Development and Testing
+
+Tested on Python 3.6, which is used in CentOS 7 (used on ONF Gerrit instances).
+
+Only built-in Python libraries are used, to eliminate external dependencies.
+
+To test changes, run `tox` in this directory. This will lint the code, run a
+full set of unit tests, and output coverage information.
---
+++ gerrit/test_ref_update.py
@@ -0,0 +1,206 @@
+# Copyright 2020-present Open Networking Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# unit tests for ref-update gerrit hook
+
+from __future__ import absolute_import
+
+import importlib.machinery
+import importlib.util
+import logging
+import os.path
+import re
+import sys
+import unittest
+import urllib.request  # noqa: F401
+import urllib.error
+from unittest.mock import patch, MagicMock
+
+# load the ref-update script as a module, hard because it doesn't end in .py
+test_dir = os.path.dirname(os.path.realpath(__file__))
+mod_path = os.path.abspath(os.path.join(test_dir, "ref-update"))
+mod_name = "refupdate"
+
+importlib.util.spec_from_file_location(mod_name, mod_path)
+spec = importlib.util.spec_from_loader(
+            mod_name,
+            importlib.machinery.SourceFileLoader(mod_name, mod_path)
+        )
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
+sys.modules[mod_name] = module
+import refupdate  # noqa: E402
+
+# set logging, using same logger as script
+logger = logging.getLogger('ref-update')
+logger.setLevel(logging.DEBUG)
+
+
+class refupdate_test(unittest.TestCase):
+
+    def test_uploader_email_good1(self):
+        '''
+        Pass with email that has fullname prefix as input
+        '''
+        input_addr = "Foo Bar (foo@bar.baz)"
+
+        output = refupdate.uploader_email(input_addr)
+        self.assertEqual(output, "foo@bar.baz")
+
+    def test_uploader_email_good2(self):
+        '''
+        Pass with bare email address as input
+        '''
+        input_addr = "foo@bar.baz"
+
+        output = refupdate.uploader_email(input_addr)
+        self.assertEqual(output, "foo@bar.baz")
+
+    def test_uploader_email_invalid(self):
+        '''
+        Fail when non-email as input
+        '''
+        input_addr = "not an email address"
+
+        with self.assertRaises(SystemExit) as cm:
+            refupdate.uploader_email(input_addr)
+
+        # check exit code depening on ENFORCING value
+        if refupdate.ENFORCING:
+            self.assertEqual(cm.exception.code, 1)
+        else:
+            self.assertEqual(cm.exception.code, 0)
+
+    @patch('urllib.request.urlopen')
+    def test_request_cla_status_pass(self, mock_urlopen):
+        '''
+        Pass when CLA signed
+        '''
+        cla_url = "https://cla-manager.example/check-cla"
+        addr = "foo@bar.com"
+
+        cm = MagicMock()
+        cm.getcode.return_value = 200
+        cm.read.return_value = '{ "signed": true, "message": "foo@bar.com signed the CLA" }'
+        cm.__enter__.return_value = cm
+        mock_urlopen.return_value = cm
+        (status, message) = refupdate.request_cla_status(cla_url, addr)
+        self.assertTrue(status)
+
+    @patch('urllib.request.urlopen')
+    def test_request_cla_status_fail(self, mock_urlopen):
+        '''
+        Fail when CLA not signed
+        '''
+        cla_url = "https://cla-manager.example/check-cla"
+        addr = "foo@bar.com"
+
+        cm = MagicMock()
+        cm.getcode.return_value = 200
+        cm.read.return_value = '{ "signed": false, "message": "foo@bar.com needs to sign the CLA" }'
+        cm.__enter__.return_value = cm
+        mock_urlopen.return_value = cm
+
+        (status, message) = refupdate.request_cla_status(cla_url, addr)
+        self.assertFalse(status)
+        self.assertEqual(message, "foo@bar.com needs to sign the CLA")
+
+    @patch('urllib.request.urlopen')
+    def test_request_cla_status_http_error(self, mock_urlopen):
+        '''
+        Fail on HTTP error
+        '''
+        cla_url = "https://cla-manager.example/check-cla"
+        addr = "foo@bar.com"
+
+        mock_urlopen.side_effect = urllib.error.HTTPError(cla_url, 500, 'Internal Error', {}, None)
+
+        (status, message) = refupdate.request_cla_status(cla_url, addr)
+        self.assertFalse(status)
+        self.assertTrue(re.fullmatch(r'^Server encountered an HTTPError at URL.*$', message))
+
+    @patch('urllib.request.urlopen')
+    def test_request_cla_status_URL_error(self, mock_urlopen):
+        '''
+        Fail on URL error
+        '''
+        cla_url = "https://cla-manager.example/check-cla"
+        addr = "foo@bar.com"
+
+        mock_urlopen.side_effect = urllib.error.URLError(404, 'Not Found')
+
+        (status, message) = refupdate.request_cla_status(cla_url, addr)
+        self.assertFalse(status)
+        self.assertTrue(re.fullmatch(r'^An URLError occurred at URL.*$', message))
+
+    def test_check_cla_response_pass(self):
+        '''
+        Pass CLA with positive JSON input
+        '''
+        jsondata = '{ "signed": true, "message": "foo@bar.com signed the CLA" }'
+
+        (status, message) = refupdate.check_cla_response(jsondata)
+        self.assertTrue(status)
+
+    def test_check_cla_response_fail(self):
+        '''
+        Fail CLA using negative JSON input
+        '''
+        jsondata = '{ "signed": false, "message": "foo@bar.com needs to sign the CLA" }'
+
+        (status, message) = refupdate.check_cla_response(jsondata)
+        self.assertFalse(status)
+        self.assertEqual(message, "foo@bar.com needs to sign the CLA")
+
+    def test_check_cla_response_invalid_json1(self):
+        '''
+        Fail with non-JSON as input
+        '''
+        jsondata = "invalid"
+
+        (status, message) = refupdate.check_cla_response(jsondata)
+        self.assertFalse(status)
+        self.assertEqual(message, "Unable to decode JSON")
+
+    def test_check_cla_response_invalid_json2(self):
+        '''
+        Fail when JSON that doesn't contain correct keys
+        '''
+        jsondata = '{ "signed": true }'
+
+        (status, message) = refupdate.check_cla_response(jsondata)
+        self.assertFalse(status)
+        self.assertEqual(message, "JSON didn't contain keys: 'status' and 'message'")
+
+    def test_output_status_pass(self):
+        '''
+        Pass when CLA is signed
+        '''
+        with self.assertRaises(SystemExit) as cm:
+            refupdate.output_status(True, "")
+
+        self.assertEqual(cm.exception.code, 0)
+
+    def test_output_status_fail(self):
+        '''
+        Fail when CLA is not signed
+        '''
+        with self.assertRaises(SystemExit) as cm:
+            refupdate.output_status(False, "foo@bar.com needs to sign the CLA")
+
+        # check exit code depening on ENFORCING value
+        if refupdate.ENFORCING:
+            self.assertEqual(cm.exception.code, 1)
+        else:
+            self.assertEqual(cm.exception.code, 0)
