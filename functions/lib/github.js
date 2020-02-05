3be095776079a68dde4a92c61a2709f057bf5044
Carmelo Cascone <carmelo@opennetworking.org>
2020-02-05 00:57:00 -0800

    Rework github webhook to handle requests asynchronously
    The payload of a PR webook is now stored in the DB and processed
    asynchronously. This allows us to keep track of PRs that are not covered
    by a CLA. When a new addendum is added and the whitelist updated, we can
    scan for all pending requests and process them right away, without any
    extra actions from users (TODO).

--- functions/lib/github.js
+++ functions/lib/github.js
@@ -1,14 +1,8 @@
+const util = require('./util')
+const Cla = require('./cla')
 const App = require('@octokit/app')
 const Octokit = require('@octokit/rest')
 const WebhooksApi = require('@octokit/webhooks')
-
-// const debug = false
-// const reqLogger = {
-//  debug: debug ? () => {} : console.log,
-//  info: debug ? () => {} : console.log,
-//  warn: console.warn,
-//  error: console.error
-// }
 
 module.exports = Github
 
@@ -17,151 +11,198 @@ module.exports = Github
  * @param appId {number}
  * @param privateKey {string}
  * @param secret {string}
- * @param clalib {Cla}
+ * @param db {FirebaseFirestore.Firestore}
  * @constructor
  */
-function Github (appId, privateKey, secret, clalib) {
-  const ghApp = new App({ id: appId, privateKey: privateKey })
+function Github (appId, privateKey, secret, db) {
+  const ghApp = new App({
+    id: appId,
+    // Key is passed aa firebase config string with new lines encoded as "\n".
+    // We need to replace "\n" with actual new line characters.
+    privateKey: privateKey.replace(/\\n/g, '\n')
+  })
   // const jwt = app.getSignedJsonWebToken() // global app token
   const ghWebhooks = new WebhooksApi(secret ? { secret } : {})
-
-  ghWebhooks.on('*', (event) => {
-    console.log('event received', event)
-  })
-
-  ghWebhooks.on('error', (error) => {
-    console.log('error occurred', error)
-  })
 
   ghWebhooks.on(
     [
       'pull_request.opened',
+      'pull_request.reopened',
       'pull_request.synchronize'
-    ], async context => {
+    ], context => {
+      // Store request to the db. We'll process it later.
       // TODO: check repo owner and discard if it's not managed by ONF
+      // TODO: create a request model
       const pr = context.payload.pull_request
-      const owner = pr.base.repo.owner.login
-      const repo = pr.base.repo.name
-      const prNum = pr.number
-      const eventType = context.payload.action
-      const numCommits = pr.commits
-
-      console.log(`Pull Request: ${owner}/${repo}/${prNum}, type ${eventType}, ${numCommits} commits`)
-
-      const installationId = context.payload.installation.id
-      const installationAccessToken = await ghApp.getInstallationAccessToken({ installationId })
-
-      const ghClient = new Octokit({
-        auth: installationAccessToken // jwt,
-        // log: reqLogger //FIXME
-      })
-
-      const status = {
-        owner: owner,
-        repo: repo,
-        context: 'clam',
-        sha: pr.head.sha
-      }
-
-      // TODO if numCommits <= 250, use the commits_url or pull request list
-      //  commits API else, use the repo commits API; can be an error for now
-      if (numCommits > 250) {
-        // Should post a status to the PR
-        status.state = 'failure'
-        status.target_url = 'https://sign.the.cla'
-        status.description = 'Cannot evaluate CLA for this PR. ' +
-          'Number of commits exceeds the 250 commit limit. ' +
-          'Please contact contact support@opennetworking.org'
-        return ghClient.repos.createStatus(status)
-      }
-
-      // We need a CLA in file for the PR author (github ID), as well as for all
-      // the identities associated with all commits of this PR.
-      const identities = getPrIdentities(pr, ghClient)
-
-      return clalib.checkIdentities(identities).then(result => {
-        if (result.allWhitelisted) {
-          console.log('cla is signed for all commits')
-          status.state = 'success'
-          status.description = 'All good! We have a CLA in file for all contributors in this PR.'
-        } else {
-          let msg
-          if (result.missingIdentities.length) {
-            msg = 'We could not find a CLA for the following identities: ' +
-              result.missingIdentities +
-              '. You will need to sign one before we can merge your PR.'
-          } else {
-            msg = 'We were not able to verify the CLA for this PR. ' +
-              'If the problem persists please contact support@opennetworking.org'
-          }
-          status.state = 'failure'
-          status.target_url = 'https://sign.the.cla'
-          status.description = msg
-        }
-        return ghClient.repos.createStatus(status)
-      })
+      return db.collection('requests')
+        .add({
+          contributionId: `github.com/${pr.base.repo.full_name}/pull/${pr.number}`,
+          receivedOn: new Date(),
+          type: 'github',
+          event: 'pull_request.' + context.payload.action,
+          payload: context.payload,
+          app: {
+            installationId: context.payload.installation.id
+          },
+          identities: null,
+          lastRunStatus: null,
+          lastRunOn: null,
+          processedCount: 0
+        })
+        .catch(error => {
+          console.error(error)
+        }).finally()
     })
 
-  async function getPrIdentities (pr, ghClient) {
+  /**
+   * Process a request from the database.
+   * @param requestSnapshot {DocumentSnapshot}
+   * @returns {Promise}
+   */
+  async function processRequest (requestSnapshot) {
+    const req = requestSnapshot.data()
+    console.log(`contributionId=${req.contributionId}, event=${req.event}, receivedOn=${req.receivedOn}`)
+
+    const pr = req.payload.pull_request
+
+    const installationId = req.app.installationId
+    const accessToken = await ghApp.getInstallationAccessToken({ installationId })
+    const octokit = new Octokit({ auth: accessToken })
+
+    // Status to post to GitHub. We need to determine a state and a description.
+    req.lastStatus = {
+      owner: pr.base.repo.owner.login,
+      repo: pr.base.repo.name,
+      context: 'cla-validation',
+      sha: pr.head.sha,
+      target_url: 'https://cla.opennetworking.org',
+      description: null,
+      state: null
+    }
+    req.lastProcessedOn = new Date()
+    req.processedCount = req.processedCount + 1
+
+    // Extract identities (if it's the first we process this request)
+    if (!req.identities) {
+      if (pr.commits > 250) {
+        // TODO if numCommits <= 250, use the commits_url or pull request list
+        //  commits API else, use the repo commits API; can be an error for now
+        req.lastStatus.state = 'error'
+        req.lastStatus.description = 'number of commits exceed the 250 limit'
+      } else {
+        try {
+          req.identities = await getPrIdentities(pr, octokit)
+        } catch (error) {
+          console.error(error)
+          req.lastStatus.state = 'error'
+          req.lastStatus.description = 'internal error'
+        }
+      }
+    }
+
+    // If we managed to extract identities (state is not error), verify that all
+    // identities are whitelisted.
+    if (!req.lastStatus.state) {
+      if (Array.isArray(req.identities) && req.identities.length > 0) {
+        // Check whitelist.
+        const cla = Cla(db)
+        try {
+          const checkResult = await cla.checkIdentities(
+            req.identities.map(util.identityObj))
+          if (checkResult.allWhitelisted) {
+            req.lastStatus.state = 'success'
+            req.lastStatus.description = 'All good! We have a CLA in file for all contributors in this PR.'
+          } else {
+            if (checkResult.missingIdentities.length) {
+              req.lastStatus.state = 'failure'
+              req.lastStatus.description =
+                'We could not find a CLA for the following identities: ' +
+                checkResult.missingIdentities.join(', ') +
+                '. You will need to sign one before we can accept this contribution.'
+            } else {
+              req.lastStatus.state = 'error'
+              req.lastStatus.description = 'whitelist verification failed but missing identities is empty'
+            }
+          }
+        } catch (error) {
+          console.error(error)
+          req.lastStatus.state = 'error'
+          req.lastStatus.description = 'cannot check whitelist'
+        }
+      } else if (Array.isArray(req.identities)) {
+        req.lastStatus.state = 'error'
+        req.lastStatus.description = 'empty identities'
+      } else {
+        req.lastStatus.state = 'error'
+        req.lastStatus.description = 'invalid identities'
+      }
+    }
+
+    // We should have a state to report by now.
+    if (!req.lastStatus.state) {
+      req.lastStatus.state = 'error'
+      req.lastStatus.description = 'internal error'
+    }
+
+    // If any error occurred, improve description shown to user.
+    if (req.lastStatus.state === 'error') {
+      req.lastStatus.description =
+        `We were unable to verify the CLA: ${req.lastStatus.description}. ` +
+        'If the problem persists, please contact support@opennetworking.org.'
+    }
+
+    // Post status to Github.
+    try {
+      const octoResponse = await octokit.repos.createStatus(req.lastStatus)
+      req.lastStatus.octoAck = octoResponse.status === 200
+      req.lastStatus.octoResponse = {
+        status: octoResponse.status,
+        data: octoResponse.data
+      }
+    } catch (e) {
+      req.lastStatus.octoAck = false
+      req.lastStatus.octoError = JSON.parse(JSON.stringify(e))
+    }
+
+    // Finally, update request in the db.
+    return requestSnapshot.ref.update(req)
+      .catch(error => {
+        console.log(error)
+        return Promise.reject(error)
+      })
+  }
+
+  async function getPrIdentities (pr, octokit) {
     // We need a CLA in file for the PR author (github ID), as well as for all
     // the identities associated with all commits of this PR.
-    const identities = [{ type: 'github', value: pr.user.login }]
-    const responses = await ghClient.paginate.iterator(`GET ${pr.commits_url}`)
+    const identities = [`github:${pr.user.login}`]
+    const responses = await octokit.paginate.iterator(`GET ${pr.commits_url}`)
     for await (const response of responses) {
       response.data.map(commit => getCommitIdentities(commit))
         .forEach(x => identities.push(...x))
     }
-    return identities
+    return Array.from(new Set(identities))
   }
 
   function getCommitIdentities (commit) {
-    // FIXME this is a hack for storing PR toJson
-    const match = commit.url.match(/repos\/([^/]+)\/([^/]+)/)
-    const ref = {
-      owner: match[1],
-      repo: match[2],
-      sha: commit.sha
-    }
-    console.log(ref)
-
     const identities = [
-      { type: 'github', value: commit.author.login },
-      { type: 'email', value: commit.commit.author.email }
+      `github:${commit.author.login}`,
+      `email:${commit.commit.author.email}`
     ]
-
     if (commit.committer.login !== 'web-flow') {
       identities.push(...[
-        { type: 'github', value: commit.committer.login },
-        { type: 'email', value: commit.commit.committer.email }
+        `github:${commit.committer.login}`,
+        `email:${commit.commit.committer.email}`
       ])
     }
-
-    return identities
+    return Array.from(new Set(identities))
   }
-
-  // function recheckPrsForEmails (emails) {
-  //   console.log('rechecking:', emails)
-  //   return Promise.all(emails.map(async email => {
-  //     const refs = await claClient.getPrsForEmail(email)
-  //     return refs.map(ref => {
-  //       // FIXME This is a Hack that won't work if there is more than one commit
-  //       return client.repos.createStatus({
-  //         owner: ref.owner,
-  //         repo: ref.repo,
-  //         sha: ref.sha,
-  //         context: 'cla-manager',
-  //         state: 'success',
-  //         description: 'CLA is signed'
-  //       })
-  //     })
-  //   }))
-  // }
 
   return {
     receive: ghWebhooks.receive,
     handler: ghWebhooks.middleware,
+    processRequest: processRequest,
     getPrIdentities: getPrIdentities,
     getCommitIdentities: getCommitIdentities
-    // recheckPrsForEmails
   }
 }
