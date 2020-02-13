eb78dbe7c533a09da816451148eec4c7baf1bbd9
Brian O'Connor <bocon@opennetworking.org>
2020-02-12 17:21:49 -0800

    Some touchups to wording for CLA sign flow (#78)

--- client/src/js/helpers/SignCheck.jsx
+++ client/src/js/helpers/SignCheck.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
+import ReactMarkdown from 'react-markdown'
 import PropTypes from 'prop-types'
 import AgreementContainer from '../agreement/AgreementContainer'
 import { AgreementType } from '../../common/model/agreement'
-import { Paper, Grid, Button } from '@material-ui/core'
+import { Button, Grid, Paper } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
+import { ClaRequestCorporate } from '../cla/ClaText'
 
 /**
  * Component which asks wether you are allowed to sign on behalf of your organization or not
@@ -25,10 +27,9 @@ const useStyles = makeStyles(theme => ({
 function EmailBody (props) {
   return (
     <div>
-      <h2>Copy this email and send it to someone who can sign</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt enim illum, veniam autem excepturi quibusdam
-        ratione iste fuga suscipit cupiditate, dicta perferendis dolor eum minus quaerat velit. Rem, dignissimos
-        repudiandae?</p>
+      <h2>Please email the following to someone at your company who can sign the CLA:</h2>
+      <p><i>If you don&aspos;t know who that is you can start with your manager or director.</i></p>
+      <ReactMarkdown source={ClaRequestCorporate} />
     </div>
   )
 }
@@ -36,7 +37,7 @@ function EmailBody (props) {
 function SignCheck (props) {
   const classes = useStyles()
   const [allowedToSign, setAllowedToSign] = useState(undefined)
-  
+
   if (props.agreementType === AgreementType.INDIVIDUAL) {
     return <AgreementContainer agreementType={props.agreementType} user={props.user}/>
   }
@@ -49,7 +50,11 @@ function SignCheck (props) {
 
   return (
     <Paper elevation={23} className={classes.root}>
-      <h2 className={classes.h2}>Can you sign for you company?</h2>
+      <h2 className={classes.h2}>Are you authorized to sign legal agreements for your company?</h2>
+      <p>
+        Typically, this means you are an executive (e.g. CEO or CTO) or officer that is explicitly
+        authorized by your company&apos;s board.
+      </p>
       <Grid container spacing={3} justify="center">
         <Grid item xs={12} md={6} className={classes.cell}>
           <Button variant="contained" color="primary" size="large" onClick={() => setAllowedToSign(true)}>
