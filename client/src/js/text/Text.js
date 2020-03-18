5bdd004ce58cc161928c68f8cc83283879d9824c
Carmelo Cascone <carmelo@opennetworking.org>
2020-03-18 17:01:14 -0400

    Use permalink for wiki page (#125)
    And make text more readable by not interpolating URLs in sentences

--- client/src/js/text/Text.js
+++ client/src/js/text/Text.js
@@ -5,11 +5,13 @@ If you don't know who that person is, you can start with your manager or directo
 
 Dear <manager, CTO, lawyer, or similar>,
 
-I would like to contribute to one of the Open Networking Foundation's (ONF) open source projects. This requires a Contributor License Agreement (CLA) to be signed between ONF and our company. Please find more information from the ONF CLA Information (https://wiki.opennetworking.org/display/COM/ONF+CLA+Information) page.
+I would like to contribute to one of the Open Networking Foundation's (ONF) open source projects. This requires a Contributor License Agreement (CLA) to be signed between ONF and our company. Please find more information from the ONF CLA Information page:
+https://wiki.opennetworking.org/x/BgCUI
 
 If we don't have an Institutional CLA signed with ONF, could you please sign one? It is a click-through agreement and only takes a few minutes. Then, can you please add my email address and GitHub ID(s) as listed below to our CLA?
 
-Please visit ONF CLA Manager (https://cla.opennetworking.org/) to sign the CLA.
+Please visit ONF CLA Manager to sign the CLA:
+https://cla.opennetworking.org
 
 My email to be added to the CLA: ___
 
