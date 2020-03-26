221fa2b1574a9eef2171e596a3fcfa9b79520082
Matteo <teone@users.noreply.github.com>
2020-03-26 10:43:15 -0700

    Adding Know More button in header bar, closes #116 (#136)

--- client/src/js/helpers/Header.jsx
+++ client/src/js/helpers/Header.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 import SignOutContainer from './SignOutContainer'
 import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
-import Link from '@material-ui/core/Link'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
+import { Link, Button, Toolbar, Typography } from '@material-ui/core'
 
 /**
  * Renders the standard header for pages within the CLA Manager application.
  */
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1
+    '& * + *:not(:last-child)': {
+      marginRight: theme.spacing(2)
+    }
   },
   title: {
     flexGrow: 1
   }
-})
+}))
 
 export default function Header (props) {
   const classes = useStyles()
@@ -32,6 +32,11 @@ export default function Header (props) {
               ONF CLA Manager
             </Link>
           </Typography>
+          <Link href="https://wiki.opennetworking.org/x/BgCUI" target="_blank">
+            <Button variant='contained' color='secondary'>
+              Know More
+            </Button>
+          </Link>
           {user && (
             <SignOutContainer
               user={user}
