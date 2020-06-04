f8d09749ba977d3599b2ccd86fc4922e8e14d6aa
Carmelo Cascone <carmelo@opennetworking.org>
2020-06-03 23:42:50 -0700

    Cleanups

--- functions/lib/crowd.js
+++ functions/lib/crowd.js
@@ -28,27 +28,29 @@ function Crowd (db, appName, appPassword) {
    * Validates user credentials against the Crowd server and updates the DB if
    * successful. Returns the path to the updated document under the "users"
    * collection.
+   *
+   * This function can be called from the client.
+   *
    * @param data {{username: string, password: string}} request data
    * @param context {CallableContext} firebase context
    * @return {string} user key
    */
   async function validateCredentials (data, context) {
-    // Checking that the user is authenticated.
+    // Checking that the Firebase user is authenticated.
     if (!context.auth) {
       // Throwing an HttpsError so that the client gets the error details.
       throw new functions.https.HttpsError('permission-denied',
         'The function must be called while authenticated')
     }
 
-    const uid = context.auth.uid
-    // Message text passed from the client.
-    const username = data.username || null
-    const password = data.password || null
+    const firebaseUid = context.auth.uid
+    const crowdUsername = data.username || null
+    const crowdPassword = data.password || null
 
     // Authenticate user against Crowd server
-    let session
+    let crowdSession
     try {
-      session = await createSession(username, password)
+      crowdSession = await createSession(crowdUsername, crowdPassword)
     } catch (e) {
       throw new functions.https.HttpsError('failed-precondition',
         'Crowd authentication failed')
@@ -56,21 +58,21 @@ function Crowd (db, appName, appPassword) {
 
     try {
       // Authenticated! Get user info.
-      const user = await getUser(username)
+      const crowdUser = await getUser(crowdUsername)
       const result = {
-        key: user.key,
-        username: user.name,
-        active: user.active,
-        firstName: user['first-name'],
-        lastName: user['last-name'],
-        displayName: user['display-name'],
-        email: user.email,
+        key: crowdUser.key,
+        username: crowdUser.name,
+        active: crowdUser.active,
+        firstName: crowdUser['first-name'],
+        lastName: crowdUser['last-name'],
+        displayName: crowdUser['display-name'],
+        email: crowdUser.email,
         updatedOn: new Date()
       }
       // We got what we needed. User logout.
-      invalidateSession(session.token).catch(console.error)
+      invalidateSession(crowdSession.token).catch(console.error)
       // Update db.
-      const docPath = `${uid}/${crowdServer}/${result.key}`
+      const docPath = `${firebaseUid}/${crowdServer}/${result.key}`
       await db.collection('users')
         .doc(docPath)
         .set(result)
@@ -79,39 +81,6 @@ function Crowd (db, appName, appPassword) {
       console.log(e)
       throw new functions.https.HttpsError('internal',
         'An internal error occurred while evaluating the request')
-    }
-  }
-
-  /**
-   * Validates the given credentials against the Crowd server and returns an
-   * object describing the user, or null if the authentication was not
-   * successful.
-   * @param username {string} crowd username
-   * @param password {string} crowd password
-   * @return {Promise<{firstName: *, lastName: *, displayName: *, active: *,
-   *   key: *, email: *, username: *}|null>} otherwise
-   */
-  async function validateCretendials (username, password) {
-    try {
-      const session = await createSession(username, password)
-      // console.log(session.token)
-      const user = await getUser(username)
-      // console.log(user)
-      const result = {
-        key: user.key,
-        username: user.name,
-        active: user.active,
-        firstName: user['first-name'],
-        lastName: user['last-name'],
-        displayName: user['display-name'],
-        email: user.email,
-        updatedOn: new Date()
-      }
-      await invalidateSession(session.token)
-      return result
-    } catch (e) {
-      console.log(e)
-      return null
     }
   }
 
