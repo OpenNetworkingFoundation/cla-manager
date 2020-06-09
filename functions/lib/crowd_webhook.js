b7d227cd8548be3b2d3910de8433cbf88fcfce05
Brian O'Connor <bocon@opennetworking.org>
2020-06-09 01:57:29 -0700

    Adding support for crowd webhook
    This patch focuses on syncing Github Teams with
    Crowd groups when group membership changes or a
    user's Github ID changes.

---
+++ functions/lib/crowd_webhook.js
@@ -0,0 +1,87 @@
+module.exports = CrowdWebhook
+
+function hasProperty (object, property) {
+  return Object.prototype.hasOwnProperty.call(object, property)
+}
+
+function CrowdWebhook (groupMappings, github) {
+  if (!groupMappings) {
+    // Use default mapping if one is not provided
+    groupMappings = require('./crowd_webhook_config')
+  }
+
+  async function processMembership (user, group, filter, add) {
+    const noFilter = !Array.isArray(filter) || !filter.length
+    const mappings = groupMappings[group]
+    for (const i in mappings) {
+      const mapping = mappings[i]
+      if (hasProperty(mapping, 'githubOrg') && // handle Github membership
+              (noFilter || filter.includes('github')) &&
+              hasProperty(user, 'githubId')) {
+        try {
+          if (add) {
+            console.log(`Adding ${user.githubId} to ${mapping.githubOrg}:${mapping.team}`)
+            // TODO: save some API calls by not always calling this
+            await github.createTeam(mapping.githubOrg, mapping.team)
+            await github.addUser(user.githubId, mapping.githubOrg, mapping.team)
+            // TODO: handle response?
+          } else {
+            console.log(`Removing ${user.githubId} from ${mapping.githubOrg}:${mapping.team}`)
+            await github.deleteUser(user.githubId, mapping.githubOrg, mapping.team)
+            // TODO: handle response?
+          }
+        } catch (e) {
+          console.log(e)
+        }
+      } // else if gerrit mapping
+      // TODO: implement this
+    }
+  }
+
+  async function addMembership (user, group, filter) {
+    await processMembership(user, group, filter, true)
+  }
+
+  async function removeMembership (user, group, filter) {
+    await processMembership(user, group, filter, false)
+  }
+
+  async function processEvent (event) {
+    if (event.type === 'USER_ADDED' || event.type === 'USER_ADDED_GITHUB') {
+      const filter = (event.type === 'USER_ADDED_GITHUB') ? ['github'] : []
+      if (event.user && event.user.groups) {
+        for (const group of event.user.groups) {
+          await addMembership(event.user, group, filter)
+        }
+      }
+    } else if (event.type === 'USER_UPDATED_GITHUB') {
+      if (event.user && event.user.groups) {
+        for (const group of event.user.groups) {
+          await removeMembership({ githubId: event.oldGithubId }, group, ['github'])
+          await addMembership(event.user, group, ['github'])
+        }
+      }
+    } else if (event.type === 'USER_DELETED_GITHUB') {
+      if (event.user && event.user.groups) {
+        for (const group of event.user.groups) {
+          await removeMembership({ githubId: event.oldGithubId }, group, ['github'])
+        }
+      }
+    } else if (event.type === 'USER_ADDED_GROUP') {
+      await addMembership(event.user, event.groupName, [])
+    } else if (event.type === 'USER_DELETED_GROUP') {
+      await removeMembership(event.user, event.groupName, [])
+    } else if (event.type === 'USER_DELETED') {
+      for (const group in groupMappings) {
+        // this approach is a bit brute force, and will attempt to remove user from all known groups
+        await removeMembership({ githubId: event.oldGithubId, email: event.oldEmail }, group, [])
+      }
+    } else {
+      // don't care about this event
+    }
+  }
+
+  return {
+    processEvent: processEvent
+  }
+}
