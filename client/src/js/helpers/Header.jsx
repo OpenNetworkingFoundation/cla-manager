1434535dd02827452d24a7aa9c7b83d6e79c8071
Matteo Scandolo <matteo.scandolo@gmail.com>
2020-11-23 12:14:55 -0800

    Changing HomePage button layout in header.jsx to make it more obvious it's a button

--- client/src/js/helpers/Header.jsx
+++ client/src/js/helpers/Header.jsx
@@ -5,7 +5,7 @@ import AppBar from '@material-ui/core/AppBar'
 import PropTypes from 'prop-types'
 import AdminNav from '../admin/AdminNavigation'
 import { Link, Button, Toolbar, Typography } from '@material-ui/core'
-import MenuIcon from '@material-ui/icons/Menu'
+import HomeIcon from '@material-ui/icons/Home'
 
 /**
  * Renders the standard header for pages within the CLA Manager application.
@@ -31,8 +31,13 @@ function Header (props) {
       <AppBar position='static'>
         <Toolbar>
           <Typography variant='h6' className={classes.title}>
-            <Link href='/' color='inherit'>
-              ONF CLA Manager
+            <Link href="/">
+              <Button
+                variant='contained'
+                color={'secondary'}
+                startIcon={<HomeIcon/>}>
+                ONF CLA Manager
+              </Button>
             </Link>
           </Typography>
           <Link href="https://wiki.opennetworking.org/x/BgCUI" target="_blank">
