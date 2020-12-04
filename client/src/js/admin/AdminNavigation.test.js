3f4cd588f44af56d37b691932815c26ed5c9a231
Matteo Scandolo <matteo.scandolo@gmail.com>
2020-12-03 17:49:32 -0800

    Fixing UI tests

--- client/src/js/admin/AdminNavigation.test.js
+++ client/src/js/admin/AdminNavigation.test.js
@@ -22,7 +22,7 @@ describe('AdminNav Component Test Suite', () => {
   it('renders one button and one menu with two items', () => {
     expect(wrapper.find(Button)).toHaveLength(1)
     expect(wrapper.find(Menu)).toHaveLength(1)
-    expect(wrapper.find(MenuItem)).toHaveLength(2)
+    expect(wrapper.find(MenuItem)).toHaveLength(3)
   })
 
   describe('when clicking on a menu item', () => {
